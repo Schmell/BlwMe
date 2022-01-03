36badd587db68a213c64eb7d57022d5a12243351
Schmell <schmell_ya_later@hotmail.com>
2022-01-03 13:12:24 -0500

    working on different result type files

--- www/sw.js
+++ www/sw.js
@@ -1,265 +1,93 @@
-const version = 5;
-let staticName = `staticCache-${version}`;
-let dynamicName = `dynamicCache`;
-let imageName = `imageCache-${version}`;
-let options = {
-  ignoreSearch: false,
-  ignoreMethod: false,
-  ignoreVary: false,
-};
-//starter html and css and js files
-let assets = [
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+// Names of the two caches used in this version of the service worker.
+// Change to v2, etc. when you update any of the local resources, which will
+// in turn trigger the install event again.
+const PRECACHE = "precache-v1";
+const RUNTIME = "runtime";
+
+// A list of local resources we always want to be cached.
+// const PRECACHE_URLS = [
+//   'index.html',
+//   './', // Alias for index.html
+//   'styles.css',
+//   '../../styles/main.css',
+//   'demo.js'
+// ];
+const PRECACHE_URLS = [
   "/",
-  "/www/index.html",
-  "/www/localStorage.html",
-  "/www/css/racing.css",
-  "/www/css/index.css",
-  "/www/js/papaparse.min.js",
-  "/www/js/importFile.js",
+  "index.html",
+  "localStorage.html",
+  "css/racing.css",
+  "css/index.css",
+  "js/papaparse.min.js",
+  "js/importFile.js",
 ];
-// "/www/racing.html", "/www/js/app.js",
-//starter images
-let imageAssets = [];
 
-let DB = null;
-
-self.addEventListener("install", (ev) => {
-  // service worker has been installed.
-  //Extendable Event
-  console.log(`Version ${version} installed`);
-  // build a cache
-  ev.waitUntil(
+// The install handler takes care of precaching the resources we always need.
+self.addEventListener("install", (event) => {
+  event.waitUntil(
     caches
-      .open(staticName)
-      .then((cache) => {
-        cache.addAll(assets).then(
-          () => {
-            //addAll == fetch() + put()
-            // console.log(`${staticName} has been updated.`);
-          },
-          (err) => {
-            console.warn(`failed to update ${staticName}.`);
-          }
+      .open(PRECACHE)
+      .then((cache) => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+  );
+});
+
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+  const currentCaches = [PRECACHE, RUNTIME];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
         );
       })
-      .then(() => {
-        caches.open(imageName).then((cache) => {
-          cache.addAll(imageAssets).then(
-            () => {
-              console.log(`${imageName} has been updated.`);
-            },
-            (err) => {
-              console.warn(`failed to update ${staticName}.`);
-            }
-          );
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
+
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
+self.addEventListener("fetch", (event) => {
+  // Skip cross-origin requests, like those for Google Analytics.
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches.match(event.request).then((cachedResponse) => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.open(RUNTIME).then((cache) => {
+          return fetch(event.request).then((response) => {
+            // Put a copy of the response in the runtime cache.
+            return cache.put(event.request, response.clone()).then(() => {
+              return response;
+            });
+          });
         });
       })
-  );
-});
-
-self.addEventListener("activate", (ev) => {
-  // when the service worker has been activated to replace an old one.
-  //Extendable Event
-  console.log("activated");
-  // delete old versions of caches.
-  ev.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(
-        keys
-          .filter((key) => {
-            if (key != staticName && key != imageName) {
-              return true;
-            }
-          })
-          .map((key) => caches.delete(key))
-      ).then((empties) => {
-        //empties is an Array of boolean values.
-        //one for each cache deleted
-        //TODO:
-        openDB();
-      });
-    })
-  );
-});
-
-self.addEventListener("fetch", (ev) => {
-  // Extendable Event.
-  ev.respondWith(
-    caches.match(ev.request).then((cacheRes) => {
-      return (
-        cacheRes ||
-        Promise.resolve().then(() => {
-          let opts = {
-            mode: ev.request.mode, //cors, no-cors, same-origin, navigate
-            cache: "no-cache",
-          };
-          if (!ev.request.url.startsWith(location.origin)) {
-            //not on the same domain as my html file
-            opts.mode = "cors";
-            opts.credentials = "omit";
-          }
-          return fetch(ev.request.url, opts).then(
-            (fetchResponse) => {
-              //we got a response from the server.
-              if (fetchResponse.ok) {
-                return handleFetchResponse(fetchResponse, ev.request);
-              }
-              //not ok 404 error
-              if (fetchResponse.status == 404) {
-                if (ev.request.url.match(/\.html/i)) {
-                  return caches.open(staticName).then((cache) => {
-                    return cache.match("/404.html");
-                  });
-                }
-                if (
-                  ev.request.url.match(/\.jpg$/i) ||
-                  ev.request.url.match(/\.png$/i)
-                ) {
-                  return caches.open(imageName).then((cache) => {
-                    return cache.match("/img/distracted-boyfriend.jpg");
-                  });
-                }
-              }
-            }, // fetch then
-            (err) => {
-              //this is the network failure
-              //return the 404.html file if it is a request for an html file
-              if (ev.request.url.match(/\.html/i)) {
-                return caches.open(staticName).then((cache) => {
-                  return cache.match("/404.html");
-                });
-              }
-            }
-          );
-        })
-      );
-    }) //end of match().then()
-  ); //end of respondWith
-}); //end of fetch listener
-
-const handleFetchResponse = async (fetchResponse, request) => {
-  let type = fetchResponse.headers.get("content-type");
-  // console.log('handle request for', type, request.url);
-  if (type && type.match(/^image\//i)) {
-    //save the image in image cache
-    // console.log(`SAVE ${request.url} in image cache`);
-    return caches.open(imageName).then((cache) => {
-      cache.put(request, fetchResponse.clone());
-      return fetchResponse;
-    });
-  } else {
-    //save in dynamic cache - html, css, fonts, js, etc
-    // console.log(`SAVE ${request.url} in dynamic cache`);
-    return caches.open(dynamicName).then((cache) => {
-      cache.put(request, fetchResponse.clone());
-      return fetchResponse;
-    });
-  }
-};
-
-self.addEventListener("message", (ev) => {
-  let data = ev.data;
-  //console.log({ ev });
-  let clientId = ev.source.id;
-  // console.log('Service Worker received', data, clientId);
-  if ("addPerson" in data) {
-    //TODO: really do something with the data
-    //TODO: open the database and wait for success
-    //TODO: start a transaction
-    //TODO: put() the data
-    //console.log({ DB });
-    //TODO: check for db then openDB or savePerson
-    if (DB) {
-      savePerson(data.addPerson, clientId);
-    } else {
-      openDB(() => {
-        savePerson(data.addPerson, clientId);
-      });
-    }
-  }
-
-  if ("otherAction" in data) {
-    let msg = "Hola";
-    sendMessage({
-      code: 0,
-      message: msg,
-    });
-  }
-});
-
-const savePerson = (person, clientId) => {
-  if (person && DB) {
-    let tx = DB.transaction("colorStore", "readwrite");
-    tx.onerror = (err) => {
-      //failed transaction
-    };
-    tx.oncomplete = (ev) => {
-      //finished saving... send the message
-      let msg = "Thanks. The data was saved.";
-      sendMessage(
-        {
-          code: 0,
-          message: msg,
-          savedPerson: person,
-        },
-        clientId
-      );
-    };
-    let store = tx.objectStore("colorStore");
-    let req = store.put(person);
-    req.onsuccess = (ev) => {
-      //saved the person
-      //tx.commit() will be called automatically
-      //and will trigger tx.oncomplete next
-    };
-  } else {
-    let msg = "No data was provided.";
-    sendMessage(
-      {
-        code: 0,
-        message: msg,
-      },
-      clientId
     );
   }
-};
-
-const sendMessage = async (msg, clientId) => {
-  let allClients = [];
-  if (clientId) {
-    let client = await clients.get(clientId);
-    allClients.push(client);
-  } else {
-    allClients = await clients.matchAll({ includeUncontrolled: true });
-  }
-  return Promise.all(
-    allClients.map((client) => {
-      // console.log('postMessage', msg, 'to', client.id);
-      return client.postMessage(msg);
-    })
-  );
-};
-
-const openDB = (callback) => {
-  let req = indexedDB.open("colorDB", version);
-  req.onerror = (err) => {
-    //could not open db
-    console.warn(err);
-    DB = null;
-  };
-  req.onupgradeneeded = (ev) => {
-    let db = ev.target.result;
-    if (!db.objectStoreNames.contains("colorStore")) {
-      db.createObjectStore("colorStore", {
-        keyPath: "id",
-      });
-    }
-  };
-  req.onsuccess = (ev) => {
-    DB = ev.target.result;
-    console.log("db opened and upgraded as needed");
-    if (callback) {
-      callback();
-    }
-  };
-};
+});
